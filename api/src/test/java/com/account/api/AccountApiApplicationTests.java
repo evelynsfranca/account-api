938f08db121917c13810344b278dfcc5f85e268e
Evelyn França <evelynsfrancaa@gmail.com>
2025-04-08 16:44:52 -0300

    chore: initial config

---
+++ api/src/test/java/com/account/api/AccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
